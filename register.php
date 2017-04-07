90de41bb35c58439dd043e31ed11d16507978640
SinedEm <denis.muzicka@gmail.com>
2017-04-07 14:04:39 +0200

    Expanded Validation security, added hover text and danger-text to register.php

--- register.php
+++ register.php
@@ -4,14 +4,14 @@
 	Helper::getHeader('Algebra Contacts');
 	
 	$validation = new Validation();
-	
-	
+
 	if(Input::exists()) {
 		$validate = $validation->check(array(
 			'name'            => array(
 				'required' => true,
 				'min'      => 2,
-				'max'      => 50
+				'max'      => 50,
+				'special'  => true
 			),
 			'username'        => array(
 				'required' => true,
@@ -21,13 +21,18 @@
 			),
 			'password'        => array(
 				'required' => true,
-				'min'      => 8
+				'min'      => 8,
+				'number'   => true,
+				'capital'  => true
 			),
 			'password_again'  => array(
 				'required' => true,
 				'matches'  => 'password'
 			)
 		));
+		echo '<pre>';
+		print_r($validate->errors());
+		echo '</pre>';
 	}
 ?>
 <div class="row">
@@ -38,22 +43,25 @@
 			</div>
 			<div class="panel-body">
 				<form method="post">
-					<div class="form-group <?php echo ($validation->hasError('name')) ? 'has-error' : ''; ?>">
+					<div class="form-group <?php echo ($validation->hasError('name')) ? 'has-error' : ''; ?>" title="Note: Your name cannot containt special characters.">
 						<label for="name" class="control-label">Name*</label>
 						<input type="text" class="form-control" id="name" name="name" placeholder="Enter your name" value="<?php echo escape(Input::get('name'))?>">
 						<?php echo ($validation->hasError('name')) ? '<p class="text-danger">'.$validation->hasError('name').'</p>' : '';?>
 					</div>
-					<div class="form-group">
+					<div class="form-group <?php echo ($validation->hasError('username')) ? 'has-error' : ''; ?>">
 						<label for="username" class="control-label">Username*</label>
-						<input type="text" class="form-control" id="username" name="username" placeholder="Enter your username">
+						<input type="text" class="form-control" id="username" name="username" placeholder="Enter your username" value="<?php echo escape(Input::get('username'))?>">
+						<?php echo ($validation->hasError('username')) ? '<p class="text-danger">'.$validation->hasError('username').'</p>' : '';?>
 					</div>
-					<div class="form-group">
+					<div class="form-group <?php echo ($validation->hasError('password')) ? 'has-error' : ''; ?>" title="Your password should contain at least one number and one uppercase letter.">
 						<label for="password" class="control-label">Password*</label>
-						<input type="password" class="form-control" id="password" name="password" placeholder="Choose a password">
+						<input type="password" class="form-control" id="password" name="password" placeholder="Choose a password" value="<?php echo escape(Input::get('password'))?>">
+						<?php echo ($validation->hasError('password')) ? '<p class="text-danger">'.$validation->hasError('password').'</p>' : '';?>
 					</div>
-					<div class="form-group">
+					<div class="form-group <?php echo ($validation->hasError('password_again')) ? 'has-error' : ''; ?>">
 						<label for="password_again" class="control-label">Confirm Password*</label>
-						<input type="password" class="form-control" id="password_again" name="password_again" placeholder="Enter your password again">
+						<input type="password" class="form-control" id="password_again" name="password_again" placeholder="Enter your password again" value="<?php echo escape(Input::get('password_again'))?>">
+						<?php echo ($validation->hasError('password_again')) ? '<p class="text-danger">'.$validation->hasError('password_again').'</p>' : '';?>
 					</div>
 					<div class="form-group">
 						<button type="submit" class="btn btn-primary">Create an account</button>
